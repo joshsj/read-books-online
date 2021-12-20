5b9fc8974a6989fbed0414a94b6d3e42b14f597f
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2021-12-20 09:36:32 +0000

    Improve logger

--- backend/src/dependency/index.ts
+++ backend/src/dependency/index.ts
@@ -1,6 +1,6 @@
 const Dependency = Object.freeze({ logger: Symbol(), env: Symbol() } as const);
 
-type LoggerContext = "general" | "server";
+type LoggerContext = "general" | "init" | "server";
 type Logger = (context: LoggerContext, data: any, ...rest: any[]) => void;
 
 export { Dependency, Logger };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,19 +4,21 @@ import { Dependency, Logger } from "./dependency";
 import { Env, getEnv } from "./env";
 import { startServer } from "./server";
 
-// TODO: consider moving
-const logger: Logger = (context, data: any, ...rest: any[]) =>
-  console.log(`[${context}] ${[data, ...rest]}`);
+const main = async () => {
+  const env = getEnv();
+  // TODO: consider moving
+  const logger: Logger = (context, data: any, ...rest: any[]) =>
+    console.log(
+      `[${context}] ${[data, ...rest]
+        .map((x) => (typeof x === "object" ? JSON.stringify(x) : x))
+        .join(", ")}`
+    );
 
-const configureDependencies = (env: Env) =>
   container
     .register<Logger>(Dependency.logger, { useValue: logger })
     .register<Env>(Dependency.env, { useValue: env });
 
-const main = async () => {
-  const env = getEnv();
-
-  configureDependencies(env);
+  logger("init", "Using env", env);
 
   startServer();
 };
