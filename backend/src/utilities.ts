6213f1b8a2f0cf731a7092575002b4b1abde32d6
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2021-12-21 16:22:03 +0000

    Add guard utility

---
+++ backend/src/utilities.ts
@@ -0,0 +1,12 @@
+type Guard = (condition: boolean, error?: string | Error) => asserts condition;
+
+const guard: Guard = (
+  condition: boolean,
+  error?: string | Error
+): asserts condition => {
+  if (!condition) {
+    throw typeof error === "string" ? new Error(error) : error;
+  }
+};
+
+export { guard, Guard };
