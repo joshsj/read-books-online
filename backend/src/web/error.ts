19cae7c9cc349686708f0d8f099a7693ed679869
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2022-01-03 19:15:06 +0000

    Remove web error

--- backend/src/web/error.ts
+++
@@ -1,9 +0,0 @@
-type ApiErrorType = "validation";
-
-class ApiError extends Error {
-  constructor(public readonly type: ApiErrorType, message: string) {
-    super(message);
-  }
-}
-
-export { ApiError, ApiErrorType };
--- backend/src/web/middlewares/errorHandler.ts
+++ backend/src/web/middlewares/errorHandler.ts
@@ -1,30 +1,21 @@
 import { ILogger } from "@/application/common/interfaces";
 import { Dependency } from "@/application/dependency";
-import { ApiError, ApiErrorType } from "@/web/error";
+import { EntityValidationError } from "@/domain/error/entityValidationError";
 import { ErrorRequestHandler } from "express";
 import { container } from "tsyringe";
-
-const defaultErrorData = [
-  "Unknown error occurred",
-  500,
-  "Internal error occurred",
-] as const;
-
-const statusCodes: { [K in ApiErrorType]: number } = {
-  validation: 400,
-};
 
 // Function must be declared with all 4 arguments to be understood by Express
 const errorHandler: ErrorRequestHandler = (err, {}, res, {}) => {
   const log = container.resolve<ILogger>(Dependency.logger);
 
-  const [logMessage, status, message] =
-    err instanceof ApiError
-      ? ["API error occurred", statusCodes[err.type], err.message]
-      : defaultErrorData;
+  if (err instanceof EntityValidationError) {
+    res.status(400).send(err.message);
+    log("server", "Entity Validation error occurred");
+    return;
+  }
 
-  log("server", logMessage, err);
-  res.status(status).send(message);
+  log("server", "Unknown error occurred");
+  res.status(500).send("Internal error occurred");
 };
 
 export { errorHandler };
