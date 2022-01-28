54284e5c9526af5bc07492a6e06b5f3799d9b72c
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2022-01-28 01:34:10 +0000

    Add truncation to logger

--- packages/backend/src/infrastructure/logger.ts
+++ packages/backend/src/infrastructure/logger.ts
@@ -1,10 +1,11 @@
 import { IHttpContextService } from "@backend/application/common/interfaces/httpContextService";
 import { ILogger, ILoggerContext } from "@backend/application/common/interfaces/logger";
+import { truncate } from "@core/utilities/string";
 import { EOL } from "os";
 
 const bracket = (s: string) => `[${s}]`;
-const pretty = (data: any) =>
-  data ? (typeof data === "object" ? JSON.stringify(data) : data) : "";
+const pretty = (data: any): string =>
+  truncate(data ? (typeof data === "object" ? JSON.stringify(data) : data) : "", 50);
 
 class Logger implements ILogger {
   constructor(private readonly httpContextService?: IHttpContextService) {}
